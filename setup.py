93f8190e2b60f6577e980c4b2ec56979de97d0cf
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-02-02 19:41:08 +0100

    DB 11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     description = fh.read()
 try:
-    originalFile = './dbiu_databases/ladepunkte.db'  # modify this line to choose a database file
+    originalFile = './dbiu_databases/laenderspiele.db'  # modify this line to choose a database file
     dbFile = './dbiu_databases/base.db'
     bakFile = dbFile+'.bak'
 
@@ -16,7 +16,7 @@ try:
 
     setup(
         name='dbiu_databases',
-        version='10',  # modify this line to set a package version, one version for each DB
+        version='11',  # modify this line to set a package version, one version for each DB
         packages=find_packages(),
         install_requires=[
         ],
